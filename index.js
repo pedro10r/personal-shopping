2e7d1f29d0b6170de969a399142ef5d16d2d8318
pedro10r <pedrosantos.ti.web@gmail.com>
2022-11-15 20:23:25 -0300

    feat: add func update and delete & config firestore emulator

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import { registerRootComponent } from 'expo';
 
+import '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+
+if(__DEV__) {
+  firestore().terminate().then(() => {
+    firestore().clearPersistence().then(() => {
+      firestore().useEmulator('localhost', 8080);
+    }).catch(() => {
+      console.log('Clear persistence error');
+    });
+  }).catch(() => {
+    console.log('Terminate error');
+  });
+}
+
+firestore();
+
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
